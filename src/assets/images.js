56299d6f4049c6dcec9aa8978c1b88b261c267b4
MihirGupta07 <mihirgupta0712@gmail.com>
2026-05-25 16:25:33 +0530

    Add meta image asset for improved social media sharing

--- src/assets/images.js
+++ src/assets/images.js
@@ -6,11 +6,26 @@ import cashiraImage from './cashira.png';
 import vocaprepImage from './vocaprep.png';
 import amiokayImage from './amiokay.png';
 import resumePdf from './resume.pdf';
+import kairosImage from './kairos.png';
+import qestraImage from './qestra.png';
+import wrapshareImage from './wrapshare.png';
+import logoLight from './logo-light.png';
+import logoDark from './logo-dark.png';
+import logoFavicon from './logo-favicon.png';
+import metaImage from './meta-image.png';
 export {
   mihirPhoto,
   backgroundImage,
   cashiraImage,
   vocaprepImage,
   amiokayImage,
-  resumePdf
+  resumePdf,
+  kairosImage,
+  qestraImage,
+  wrapshareImage,
+  logoLight,
+  logoDark,
+  logoFavicon,
+  metaImage
+
 };
